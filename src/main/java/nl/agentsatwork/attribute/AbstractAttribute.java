0ded9d9cb1575db0c5edbe9d53315b5f8980da4c
mailjeroen <mailjeroen@gmail.com>
2011-04-11 11:45:36 +0200

    start redesign of Attribute

--- src/main/java/nl/agentsatwork/attribute/AbstractAttribute.java
+++ src/main/java/nl/agentsatwork/attribute/AbstractAttribute.java
@@ -1,23 +1,33 @@
 package nl.agentsatwork.attribute;
 
-import nl.agentsatwork.attributes.Entity;
+import nl.agentsatwork.aggregates.AbstractEntity;
+import nl.agentsatwork.aggregates.Aggregate;
 
-public class AbstractAttribute {
+public class AbstractAttribute extends AbstractEntity implements Attribute {
 
-	final static public Entity defaultEntity = new DefaultEntityImpl();
-
-	public AbstractAttribute(String key) {
-		defaultEntity.register(key, this);
-	}
-
-	public Entity getSuperior() {
+	public String getKey() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public void setEntity(Entity superior) {
-		if (superior != null) {
-			superior.register(this);
-		}
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String setValue(String value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Aggregate getAggregate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String defaultValue() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
