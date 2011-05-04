60a7bd910577f557d62ff315966f8ae581a76b89
mailjeroen <mailjeroen@gmail.com>
2011-05-04 16:34:09 +0200

    unit test for AbstractEntity

---
+++ src/test/java/nl/agentsatwork/aggregates/AbstractEntityTest.java
@@ -0,0 +1,108 @@
+package nl.agentsatwork.aggregates;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AbstractEntityTest {
+
+	private Entity entity = null;
+	private Aggregate aggregate = null;
+
+	@Before
+	public void setUp() {
+		entity = new AbstractEntity() {
+			private Location location = new IndependentLocation();
+
+			public Location getLocation() {
+				return location;
+			}
+
+		};
+
+		aggregate = new AbstractAggregate() {
+			final private AggregateIndex index = new AbstractAggregateIndex() {
+
+				public int indexOf(Object value) {
+					throw new UnsupportedOperationException();
+				}
+
+			};
+
+			public Location getLocation() {
+				return null;
+			}
+
+			public AggregateIndex getIndex() {
+				return index;
+			}
+
+		};
+	}
+
+	@Test
+	public void testGetLocation() {
+		assertNull(entity.getLocation().getAggregate());
+		assertEquals(-1, entity.getLocation().getPosition());
+	}
+
+	@Test
+	public void testEnter() {
+		try {
+			entity.enter(null);
+			fail("cannot enter null");
+		} catch (IllegalArgumentException e) {
+
+		}
+
+		assertEquals(0, aggregate.getIndex().size());
+		assertEquals(0, entity.enter(aggregate));
+		assertEquals(0, entity.enter(aggregate));
+		assertSame(aggregate, entity.getLocation().getAggregate());
+		assertEquals(0, entity.getLocation().getPosition());
+		assertEquals(1, aggregate.getIndex().size());
+
+		Aggregate aggregate = new Aggregate() {
+
+			public int enter(Object entity) {
+				return 3;
+			}
+
+			public boolean leave(int position) {
+				return false;
+			}
+
+			public AggregateIndex getIndex() {
+				return null;
+			}
+
+		};
+
+		assertEquals(3, entity.enter(aggregate));
+		assertSame(aggregate, entity.getLocation().getAggregate());
+		assertEquals(3, entity.getLocation().getPosition());
+
+		assertEquals(0, this.aggregate.getIndex().size());
+
+	}
+
+	@Test
+	public void testLeave() {
+		try {
+			entity.leave(null);
+			fail("cannot leave null");
+		} catch (IllegalArgumentException e) {
+
+		}
+
+		assertTrue(entity.leave(aggregate));
+		assertEquals(0, entity.enter(aggregate));
+		assertTrue(entity.leave(aggregate));
+	}
+
+}
