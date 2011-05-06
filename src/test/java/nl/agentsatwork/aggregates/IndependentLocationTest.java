f1cdfa3a97aa9a045873b9a692ffe73d6ebf3645
mailjeroen <mailjeroen@gmail.com>
2011-05-06 15:49:39 +0200

    unit test for aggregate package (ndependentLocation)

---
+++ src/test/java/nl/agentsatwork/aggregates/IndependentLocationTest.java
@@ -0,0 +1,47 @@
+package nl.agentsatwork.aggregates;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IndependentLocationTest {
+
+	private Location location = null;
+	private Aggregate aggregate = new AbstractAggregate() {
+
+		public AggregateIndex getIndex() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public Location getLocation() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	};
+	
+	@Test
+	public void testGetAggregate() {
+		assertNull(location.getAggregate());
+	}
+
+	@Test
+	public void testSetAggregate() {
+		location.setAggregate(aggregate);
+		assertNotNull(location.getAggregate());
+		assertSame(aggregate, location.getAggregate());
+	}
+
+	@Test
+	public void testGetPosition() {
+		assertEquals(-1, location.getPosition());
+	}
+
+	@Test
+	public void testSetPosition() {
+		location.setPosition(0);
+		assertEquals(0, location.getPosition());
+	}
+
+}
